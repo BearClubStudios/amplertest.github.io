ef2f560f1b72001d65a6822823764e207e515fff
BearClubStudios <69652827+BearClubStudios@users.noreply.github.com>
2024-12-31 18:11:44 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -40,6 +40,7 @@ function webedition(){
     document.getElementById('header6').style.display = 'block';
     document.getElementById('gtabs2').style.fontWeight = '700';
     document.getElementById('gtabs2').style.borderLeft = '#008542 solid 4px';
+    document.getElementById('dropdown0').style.display = 'flex';
     document.getElementById('dropdown2.5').style.display = 'flex';
     document.getElementById('dropdown2').style.display = 'flex';
     document.getElementById('dropdown1').style.display = 'flex';
